a7a11b60748b014b5b65a72231ef93a4c82802b8
张伟 <nick18zhang@icloud.com>
2015-07-21 16:58:31 +0800

    Initial Commit

---
+++ YAScrollSegmentControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YAScrollSegmentControl
+//
+//  Created by Nick Z on 15/7/21.
+//  Copyright © 2015年 Nick Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YAScrollSegmentControl/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YAScrollSegmentControl
+//
+//  Created by Nick Z on 15/7/21.
+//  Copyright © 2015年 Nick Z. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
